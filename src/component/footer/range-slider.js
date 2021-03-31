be9fa3f92c1a6dd215ec1a686d1aa267ec5d0463
oğuzulukaya <55917459+oguz3@users.noreply.github.com>
2021-03-31 16:15:34 +0300

    Update range-slider thumb

--- src/component/footer/range-slider.js
+++ src/component/footer/range-slider.js
@@ -1,18 +1,25 @@
 import PropTypes from 'prop-types';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styles from "./range-slider.module.css";
 
 function RangeSlider({value, minvalue, maxvalue, handleChange}){
     const inputRef = useRef(null)
     const inputRefWidth = useRef(null)
+    const[decimalValue, setDecimalValue] = useState();
 
     useEffect(() => {
         const inputWidth = window.getComputedStyle(inputRef.current).width
-        inputRefWidth.current = parseInt( inputWidth.replace('px',''))
-    }, [])
+        inputRefWidth.current = parseInt(inputWidth.replace('px',''))
+    })
 
-    console.log(inputRefWidth.current)
+    useEffect(() => {
+        if(maxvalue > 1){
+            setDecimalValue((value * 1) / maxvalue);
+        }else{
+            setDecimalValue(value);
+        }
+    })
 
     const handleInputChange = (e) => {
         handleChange(parseFloat(e.target.value));
@@ -23,7 +30,7 @@ function RangeSlider({value, minvalue, maxvalue, handleChange}){
             <input ref={inputRef} type="range" onChange={handleInputChange} className={styles.range__slider} min={minvalue} max={maxvalue} step="0.01" value={value} />
             <span
                 className={styles.spanThumb}
-                style={{left: `${value * inputRefWidth.current}px`}}
+                style={{left: `${(decimalValue * inputRefWidth.current) - 3}px`}}
             >
 
             </span>
