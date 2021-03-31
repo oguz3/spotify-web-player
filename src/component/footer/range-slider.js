78cdff92d109c43a25065d9a3472ef2beb861598
oğuzulukaya <55917459+oguz3@users.noreply.github.com>
2021-03-31 14:24:43 +0300

    Added rangeSlider thumb hover

--- src/component/footer/range-slider.js
+++ src/component/footer/range-slider.js
@@ -1,8 +1,18 @@
 import PropTypes from 'prop-types';
+import { useEffect, useRef } from 'react';
 
 import styles from "./range-slider.module.css";
 
 function RangeSlider({value, minvalue, maxvalue, handleChange}){
+    const inputRef = useRef(null)
+    const inputRefWidth = useRef(null)
+
+    useEffect(() => {
+        const inputWidth = window.getComputedStyle(inputRef.current).width
+        inputRefWidth.current = parseInt( inputWidth.replace('px',''))
+    }, [])
+
+    console.log(inputRefWidth.current)
 
     const handleInputChange = (e) => {
         handleChange(parseFloat(e.target.value));
@@ -10,7 +20,13 @@ function RangeSlider({value, minvalue, maxvalue, handleChange}){
 
     return (
         <div className={styles.progressBar}>
-            <input type="range" onChange={handleInputChange} className={styles.range__slider} min={minvalue} max={maxvalue} step="0.01" value={value} />
+            <input ref={inputRef} type="range" onChange={handleInputChange} className={styles.range__slider} min={minvalue} max={maxvalue} step="0.01" value={value} />
+            <span
+                className={styles.spanThumb}
+                style={{left: `${value * inputRefWidth.current}px`}}
+            >
+
+            </span>
         </div>
     );
 }
